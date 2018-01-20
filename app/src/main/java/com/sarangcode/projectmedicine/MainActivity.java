2737824ebaa03f607409155cf5b1cd3045fa6e91
Ribesh19 <ribeshforu@gmail.com>
2018-01-20 18:10:48 +0545

    Initial commit

---
+++ app/src/main/java/com/sarangcode/projectmedicine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sarangcode.projectmedicine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
